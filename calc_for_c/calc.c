ad600a06a468a1d9b265b468f144cbc95a24857d
Realhospital <mintwlsehtro@gmail.com>
2018-05-17 18:59:52 +0900

    Basic Frame & Basic Operation
    ENG : Input error message output has been changed to a function, and we used "goto" statement because there was a problem with repeating process.
    KOR : 입력 오류 메세지 출력을 함수로 바꿨으며, 분기문에서 실수가 있는지 일단 goto문으로 대체하고자 합니다.
    JP :入力エラーメッセージ出力を艦首に替えており、バンボクムンに問題が発生してgoto文を使用しました (日本語でこれが当たるかわかりません。)

--- calc_for_c/calc.c
+++ calc_for_c/calc.c
@@ -5,15 +5,18 @@
 #include <math.h>
 #include <time.h>
 
-double calcu();
-int tcalcu();
-void scalcu();
-void line();
+double calcu(); //Basic operation
+int tcalcu(); //Time operation
+void scalcu(); //A numberical translation
+void line(); //make line
+void invaildch(); //invaild Choice
 
 int main()
 {
-	int select;
-
+	int select, select2;
+	
+	// The starting point of the main
+	back:
 	line();
 	printf("\t\tCalculator for C\n");
 	line();
@@ -27,6 +30,7 @@ int main()
 		printf("1. Basic operation\n 2. A numerical translation \n 3. Unit conversion\n 4. Time operation\n Type : *\b");
 		
 		scanf("%d", &select);
+
 		if (select == 0)
 		{
 			printf("Program Exit!\n");
@@ -35,14 +39,32 @@ int main()
 		switch (select)
 		{
 		case 1:
+			back2:
 			line();
 			printf("\t\tBasic Operation\n");
 			line();
+			printf("Select calculation type!\n");
+			printf("Enter 0 to return to the previous.\n");
+			// Similarly, the Sentinel is zero here.
+			printf(" 1. Single term calculation\n 2. Calculate two terms\n 3. Polynomial computation\n Type : *\b");
+
+			scanf("%d", &select2);
+
+			if (select2 == 0)
+			{
+				printf("Return to previous menu!\n\n");
+				goto back;
+				// go to start menu
+			}
+			switch (select2)
+			{
+			default:
+				invaildch();
+				goto back2;
+			}
 			break;
 		default:
-			// Unpresented Choice
-			printf("\n# ERROR : Input value is invalid!\a\n");
-			printf("Please select again.\n\n");
+			invaildch();
 			break;
 		}
 	}
@@ -50,12 +72,17 @@ int main()
 
 void line(void)
 {
-	// make a line 
 	for (int titleloop = 0; titleloop < 50; titleloop++)
 	{
 		printf("-");
 	}
 	printf("\n");
+}
+
+void invaildch(void)
+{
+	printf("\n# ERROR : Input value is invalid!\a\n");
+	printf("Please select again.\n\n");
 }
 
 int tcalcu(void)
